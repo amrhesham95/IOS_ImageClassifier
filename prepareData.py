5dc174d4bf51ab76dfaff500dc1012512a0b9fa9
Amr Hesham <amr.hesham@fawry.com>
2020-06-07 20:50:43 +0200

    getting our sframe (preparing the data to start making the model next)

---
+++ prepareData.py
@@ -0,0 +1,9 @@
+import turicreate as tc
+
+data = tc.image_analysis.load_images("TrainingData", with_path=True)
+
+data["label"] = data["path"].apply(lambda path: "dog" if "dog" in path else "cat")
+
+data.save("cats-dogs.sframe")
+
+data.explore()
