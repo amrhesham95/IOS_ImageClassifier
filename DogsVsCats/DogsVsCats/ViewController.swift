7362f5588539cfa2845c7b02c59196046e80fd6f
Amr Hesham <amr.hesham@fawry.com>
2020-06-08 16:19:15 +0200

    setting the UI

---
+++ DogsVsCats/DogsVsCats/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  DogsVsCats
+//
+//  Created by Brian Advent on 17.01.18.
+//  Copyright © 2018 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var checkAnimalButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classificationLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+
+    
+    @IBAction func selectImageSource(_ sender: Any) {
+        
+    }
+    
+    @IBAction func checkAnimal(_ sender: Any) {
+  
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
